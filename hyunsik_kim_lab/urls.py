5bb1f7eb75d173e767e475f718e611497efeef8e
Shim MunSeong <puterism.k@gmail.com>
2020-07-09 14:47:07 +0900

    불필요한 staticfiles_urlpatterns() 제거

--- hyunsik_kim_lab/urls.py
+++ hyunsik_kim_lab/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -33,4 +32,3 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
