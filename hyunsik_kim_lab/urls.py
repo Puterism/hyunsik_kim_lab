ea1448a9f325f3825079c3f8fa88ba94855c29dd
Shim MunSeong <puterism.k@gmail.com>
2020-07-16 23:28:00 +0900

    robots.txt 추가

--- hyunsik_kim_lab/urls.py
+++ hyunsik_kim_lab/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -29,6 +30,7 @@ urlpatterns = [
     path('admin/', include('login.urls')),
     path('research/', include('research.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
