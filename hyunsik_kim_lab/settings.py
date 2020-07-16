726b6f833a6a2f2b4d267550d547f5317ea36ca8
Shim MunSeong <puterism.k@gmail.com>
2020-07-16 20:16:58 +0900

    pointColor 변경 및 정상 적용

--- hyunsik_kim_lab/settings.py
+++ hyunsik_kim_lab/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hyunsik_kim_lab',
-        'USER': 'puterism',
+        'USER': 'root',
         'PASSWORD': get_secret("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '3306',
@@ -157,6 +157,13 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+# SASS
+SASS_PROCESSOR_ENABLED = True
+SASS_ROOT = os.path.join(BASE_DIR, 'static')
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 # Secure-only cookie
 
 CSRF_COOKIE_SECURE = False
@@ -165,14 +172,6 @@ SESSION_COOKIE_SECURE = False
 SECURE_HSTS_SECONDS = 31536000  # 365 * 24 * 60 * 60
 SECURE_HSTS_PRELOAD = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-
-# SASS
-SASS_ROOT = os.path.join(BASE_DIR, 'static')
-SASS_PROCESSOR_ENABLED = True
-SASS_PROCESSOR_ROOT = STATIC_ROOT
-SASS_OUTPUT_STYLE = 'compact'
-
 
 
 # Media Settings
