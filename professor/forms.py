faa42bb210b8fc9bd5a849156bad6e76d3f87740
Shim MunSeong <Puterism.K@gmail.com>
2020-07-22 00:37:54 +0900

    Professor ProfileForm에 resize 적용 및 보안 설정 추가

--- professor/forms.py
+++ professor/forms.py
@@ -29,6 +29,52 @@ class ProfileForm(forms.ModelForm):
             'placeholder': '이메일',
         })
 
+    def resize(self, data, width, height, force=True):
+        from io import BytesIO
+        from PIL import Image as pil
+
+        max_width = width
+        max_height = height
+
+        input_file = BytesIO(data.read())
+        img = pil.open(input_file)
+
+        if not force:
+            img.thumbnail((max_width, max_height), pil.ANTIALIAS)
+        else:
+            src_width, src_height = img.size
+            src_ratio = float(src_width) / float(src_height)
+            dst_width, dst_height = max_width, max_height
+            dst_ratio = float(dst_width) / float(dst_height)
+
+            if dst_ratio < src_ratio:
+                crop_height = src_height
+                crop_width = crop_height * dst_ratio
+                x_offset = int(src_width - crop_width) // 2
+                y_offset = 0
+            else:
+                crop_width = src_width
+                crop_height = crop_width / dst_ratio
+                x_offset = 0
+                y_offset = int(src_height - crop_height) // 3
+            img = img.crop((x_offset, y_offset, x_offset + int(crop_width), y_offset + int(crop_height)))
+            img = img.resize((dst_width, dst_height), pil.ANTIALIAS)
+
+        image_file = BytesIO()
+        img.save(image_file, 'JPEG')
+        data.file = image_file
+        return data
+
+    def save(self, commit=True, *args, **kwargs):
+        instance = super(ProfileForm, self).save(commit=False)
+
+        if instance.photo:
+            instance.photo = self.resize(self.cleaned_data.get('photo'), 600, 600, force=True)
+        if commit:
+            instance.save()
+
+        return instance
+
     class Meta:
         model = Profile
         fields = ('name', 'position', 'location', 'email', 'photo', )
